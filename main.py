1c8e3bbe38a02a249cec7a930ff5978e75858798
Jeremy Anderson <jeremy.ken.anderson@gmail.com>
2025-04-05 23:05:44 -0700

    Stymied on process of finding the index of the first instance of an item being presented as a key in a list of dicts. Intended eventual behavior is to randomly share a second word that matches the word count of a selected word, within the book.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 from stats import get_num_words, count_characters, sort_dict, slice_by_word
 import sys
 
@@ -17,6 +18,7 @@ def main():
     print(f"Found {get_num_words(manuscript)} total words")
     print("--------- Word Analysis ---------")
     word_dict = slice_by_word(manuscript)
+    word_list = sort_dict(word_dict)
     out = False
     while not out:
         pick = input("Please input a word you want to know about. Type quit! to quit. > ")
@@ -26,6 +28,18 @@ def main():
             print(f"\"{pick.lower()}\" doesn't appear in the book at {address}")
         else:
             print(f"\"{pick.lower()}\" appears {word_dict[pick.lower()]} times.")
+            word_index = word_list.index(pick.lower())
+            word_index_down = word_index
+            word_index_up = word_index
+            sublist = []
+            while word_index_down > 0 and word_dict[word_list[word_index_down-1]] == word_dict[pick.lower()]:
+                word_index_down -= 1
+                sublist.append(word_dict[word_list[word_index_down]])
+            while word_index_up < len(word_list) - 1 and word_dict[word_list[word_index_up + 1]] == word_dict[pick.lower()]:
+                word_index_up += 1
+                sublist.append(word_dict[word_list[word_index_up]])
+            matching_word = random.choice(sublist)
+            print(f"The word {matching_word} has the same number of appearances.")
 
     # print("--------- Character Count -------")
     # letter_dict = count_characters(manuscript)
